44164983ea5c031e278f70a256d048e174ab4904
vkwho9ar <vkwho9ar@gmail.com>
2021-03-24 15:02:02 -0400

    added resume download

--- src/components/ResumeBtn.js
+++ src/components/ResumeBtn.js
@@ -21,21 +21,23 @@ function ResumeBtn() {
          onMouseEnter={handleChange}
          onMouseLeave={handleChange}
       >
-         <motion.h6
-            initial={{ y: 0 }}
-            animate={showDownload ? { y: -25 } : { y: 0 }}
-            transition={{ duration: 0.5, ease: "easeIn" }}
-         >
-            Resume
-         </motion.h6>
-         <motion.button
-            className="download"
-            initial={{ y: 25 }}
-            animate={showDownload ? { y: -25 } : { y: 0 }}
-            transition={{ duration: 0.5, ease: "easeIn" }}
-         >
-            <FontAwesomeIcon icon="download" className="icon" />
-         </motion.button>
+         <a href="/assets/vhResume.pdf" target="_blank">
+            <motion.h6
+               initial={{ y: 0 }}
+               animate={showDownload ? { y: -25 } : { y: 0 }}
+               transition={{ duration: 0.5, ease: "easeIn" }}
+            >
+               Resume
+            </motion.h6>
+            <motion.button
+               className="download"
+               initial={{ y: 25 }}
+               animate={showDownload ? { y: -25 } : { y: 0 }}
+               transition={{ duration: 0.5, ease: "easeIn" }}
+            >
+               <FontAwesomeIcon icon="download" className="icon" />
+            </motion.button>
+         </a>
       </div>
    );
 }
